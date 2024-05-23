fc71cee6dc05a0e6817a35695d6ccb29b1da14dd
David-Toelle <110549054+David-Toelle@users.noreply.github.com>
2024-05-23 13:37:06 -0400

    Add files via upload

---
+++ secure_the_vault.js
@@ -0,0 +1,25 @@
+
+// Pseudocode
+// make a message variable
+
+// make a number 1 2 and 3 variable 
+
+// add unique calculation to each variable
+
+// create an alert message
+
+// link html to js
+
+
+
+// This is the pop up message 
+let message =
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+
+// these numbers equal the combination for the vault
+let num_1 = 2*5
+let num_2 = 80 / 2
+let num_3 = 40 - 1
+
+// this creates a popup message with the combination and message
+alert(`${message} ${num_1} ${num_2} ${num_3}`);
